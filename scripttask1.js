98bfd38154b1fa9e451e4c938fbc1edc50188c6b
OlgaKoltc <Olga.Koltc@gmail.com>
2022-10-21 10:40:37 +0300

    add task1

---
+++ scripttask1.js
@@ -0,0 +1,33 @@
+let result;
+
+function add() {
+  let number1 = document.getElementById('number1').value;
+  let number2 = document.getElementById('number2').value;
+  let result = +number1 + +number2;
+  return document.getElementById('result').value = result;
+  }
+
+  function subtract() {
+    let number1 = document.getElementById('number1').value;
+    let number2 = document.getElementById('number2').value;
+    let result = +number1 - +number2;
+    return document.getElementById('result').value = result;
+    }
+
+    function multiply() {
+      let number1 = document.getElementById('number1').value;
+      let number2 = document.getElementById('number2').value;
+      let result = (Number(number1) * Number(number2));
+      return document.getElementById('result').value = result;
+      }
+      
+      function divide() {
+        let number1 = document.getElementById('number1').value;
+        let number2 = document.getElementById('number2').value;
+        let result = (Number(number1) / Number(number2));
+        if (number2 == 0) {
+          alert ("На 0 делить нельзя!");
+        }
+        return document.getElementById('result').value = result;
+        }
+        
